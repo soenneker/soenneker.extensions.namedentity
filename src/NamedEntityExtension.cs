43460aeef890bca58fc06d2d4e225492d39e3a54
Jake Soenneker <jake@soenneker.com>
2026-01-18 10:03:58 -0600

    attributes

--- src/NamedEntityExtension.cs
+++ src/NamedEntityExtension.cs
@@ -1,5 +1,7 @@
 using Soenneker.Dtos.IdNamePair;
 using Soenneker.Entities.Named.Abstract;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 
 namespace Soenneker.Extensions.NamedEntity;
 
@@ -14,6 +16,8 @@ public static class NamedEntityExtension
     /// <typeparam name="T">The type of the object that implements <see cref="INamedEntity"/>.</typeparam>
     /// <param name="value">The object to convert. Must not be null.</param>
     /// <returns>An <see cref="IdNamePair"/> containing the Id and Name of the provided object.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static IdNamePair ToIdNamePair<T>(this T value) where T : INamedEntity
     {
         return new IdNamePair {Id = value.Id, Name = value.Name};
